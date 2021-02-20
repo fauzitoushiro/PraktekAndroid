e4410e5899f70657403d1c77877ffa0df47e682a
fauzitoushiro <fauzitoushiro@gmail.com>
2021-02-20 10:25:19 +0800

    alert & dialog

--- app/src/main/java/com/praktekandroid/GameActivity.kt
+++ app/src/main/java/com/praktekandroid/GameActivity.kt
@@ -8,14 +8,23 @@ import android.os.Handler
 import kotlinx.android.synthetic.main.activity_game.*
 import java.util.*
 
+
 private const val BATU = 1
 private const val GUNTING = 2
 private const val KERTAS = 3
 
 class GameActivity : AppCompatActivity() {
+
+    companion object{
+        const val EXTRA_USER = "username"
+    }
+
+    var username:String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        username = intent.getStringExtra(EXTRA_USER).toString()
 
         var pilihanKomputer:Int
         var pilihanPlayer:Int
@@ -69,11 +78,11 @@ class GameActivity : AppCompatActivity() {
         if ((pilihanPlayer == BATU && pilihanKomputer == KERTAS) ||
            (pilihanPlayer == GUNTING && pilihanKomputer == BATU) ||
            (pilihanPlayer == KERTAS && pilihanKomputer == GUNTING)) {
-            hasil = "YOU LOSE"
+            hasil = username + " LOSE"
             textHasil.setTextColor(Color.RED)
         }
          else {
-            hasil = "YOU WIN"
+            hasil = username + " WIN"
             textHasil.setTextColor(Color.YELLOW)
         }
 
--- app/src/main/java/com/praktekandroid/MainActivity.kt
+++ app/src/main/java/com/praktekandroid/MainActivity.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import android.app.AlertDialog
+import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -28,8 +30,23 @@ class MainActivity : AppCompatActivity() {
         }
 
         button2.setOnClickListener{
-            val intent = Intent(this@MainActivity,GameActivity::class.java)
-            startActivity(intent)
+            var alert: AlertDialog.Builder = AlertDialog.Builder(this)
+            val editUser = EditText(this)
+            alert.setTitle("Username")
+            alert.setMessage("Hai")
+            alert.setView(editUser)
+
+            alert.setPositiveButton("Yes"){dialog, which ->
+                var username = editUser.text.toString()
+                val intent = Intent(this@MainActivity,GameActivity::class.java)
+                intent.putExtra(GameActivity.EXTRA_USER,username)
+                startActivity(intent)
+            }
+            alert.setNegativeButton("No"){diaglog, which ->
+
+            }
+
+            alert.show()
         }
 
     }
